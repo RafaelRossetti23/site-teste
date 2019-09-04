cd0ee9354cab9a23d3494303e0d06a6dd456506f
RafaelRossetti23 <rafaelrossetti23@gmail.com>
2019-09-04 19:48:40 -0300

    adicionado breakpoint, bootstrap-sass e font-awesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const gulp = require('gulp')
-const sass = require('gulp-ruby-sass')
+const sass = require('gulp-sass')
+const watch = require('gulp-watch')
 
 //task para o sass
 gulp.task('sass', function(){
-    return sass('sass/*.sass').pipe(gulp.dest('css'))
+    return gulp.src('sass/**/*.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('css'))
 })
-
+// task para watch
+gulp.task('watch', function(){
+    gulp.watch('sass/**/*.sass', gulp.series('sass'))
+})
 // task default gulp
-gulp.task('default', function(){
-    
-})
+gulp.task('default',gulp.series('sass', 'watch'))
