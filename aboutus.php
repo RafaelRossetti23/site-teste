c6553cbce97e282d6894d4868240dbece2c273ca
RafaelRossetti23 <rafaelrossetti23@gmail.com>
2019-09-09 00:08:57 -0300

    arrumando links

--- aboutus.php
+++ aboutus.php
@@ -1,5 +1,5 @@
 <?php 
-    $style = 'sobre.css';
+    $style = 'sobre';
     include('header.php');
     include('includes/pages/aboutus.php');
     include('footer.php');
--- header.php
+++ header.php
@@ -30,7 +30,7 @@
                         <ul>
                             <li><a href="index.php">Home</a></li>
                             <li><a href="aboutus.php">About Us</a></li>
-                            <li><a href="portifolio.php">Portifolio</a></li>
+                            <li><a href="portfolio.php">Portifolio</a></li>
                             <li><a href="contact.php">Contact</a></li>
                         </ul>
                     </nav> 
