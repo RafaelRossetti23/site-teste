efd9aa2aef4f165a511073f018dc8bcc63148c8f
RafaelRossetti23 <rafaelrossetti23@gmail.com>
2019-09-08 23:32:28 -0300

    php add

---
+++ footer.php
@@ -0,0 +1,15 @@
+</main>
+        <footer>
+            <div class="logo-rodape">
+                <a href="index.html" class="botao-link">
+                    <span><i class="fas fa-guitar"></i></span>
+                </a>
+                <p>2019 - Todos os direitos reservados &copy;</p>
+            </div>
+        </footer>
+    </div>
+    <script src="plugins/jquery/dist/jquery.min.js"></script>
+    <script src="plugins/wow/dist/wow.min.js"></script>
+    <script src="javascript/index.js"></script>
+</body>
+</html>
