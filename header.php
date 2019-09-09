32d2519b6bebc889f9880999c78e2d494cee33c1
RafaelRossetti23 <rafaelrossetti23@gmail.com>
2019-09-09 00:06:36 -0300

    mudado links do menu

--- header.php
+++ header.php
@@ -28,10 +28,10 @@
                 <div class="links">
                     <nav class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".3s">
                         <ul>
-                            <li><a href="index.html">Home</a></li>
-                            <li><a href="aboutus.html">About Us</a></li>
-                            <li><a href="portifolio.html">Portifolio</a></li>
-                            <li><a href="contact.html">Contact</a></li>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="aboutus.php">About Us</a></li>
+                            <li><a href="portifolio.php">Portifolio</a></li>
+                            <li><a href="contact.php">Contact</a></li>
                         </ul>
                     </nav> 
                     
