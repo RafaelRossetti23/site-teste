33076f4383ef76593777e762373d9caf78576faf
RafaelRossetti23 <rafaelrossetti23@gmail.com>
2019-09-13 10:51:38 -0300

    links update

--- includes/organisms/menu-responsive.php
+++ includes/organisms/menu-responsive.php
@@ -1,10 +1,10 @@
 <div class="menu-responsive">
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="sobre.php">About Us</a></li>
-            <li><a href="portfolio.php">Portifolio</a></li>
-            <li><a href="contato.php">Contact</a></li>
+            <li><a href="<?php bloginfo('template_directory'); ?>/index.php">Home</a></li>
+            <li><a href="<?php bloginfo('template_directory'); ?>/sobre.php">About Us</a></li>
+            <li><a href="<?php bloginfo('template_directory'); ?>/portfolio.php">Portifolio</a></li>
+            <li><a href="<?php bloginfo('template_directory'); ?>/contato.php">Contact</a></li>
         </ul>
     </nav>
 </div>
--- includes/organisms/menu.php
+++ includes/organisms/menu.php
@@ -1,8 +1,8 @@
 <nav class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".3s">
     <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="sobre.php">About Us</a></li>
-        <li><a href="portfolio.php">Portifolio</a></li>
-        <li><a href="contato.php">Contact</a></li>
+        <li><a href="<?php bloginfo('template_directory'); ?>/index.php">Home</a></li>
+        <li><a href="<?php bloginfo('template_directory'); ?>/sobre.php">About Us</a></li>
+        <li><a href="<?php bloginfo('template_directory'); ?>/portfolio.php">Portifolio</a></li>
+        <li><a href="<?php bloginfo('template_directory'); ?>/contato.php">Contact</a></li>
     </ul>
 </nav>
